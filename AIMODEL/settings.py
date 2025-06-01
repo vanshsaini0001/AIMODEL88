c32a1e2b58d1054b5ad98bd1b1b72157a2461cba
vanshsaini0001 <sainivansh7300@gmail.com>
2025-06-01 15:33:06 +0530

    Update settings.py

--- AIMODEL/settings.py
+++ AIMODEL/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j27s*aej-y*c*341@#ika8y%igwt&inb5^ywoi@lza1zpld*x*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
